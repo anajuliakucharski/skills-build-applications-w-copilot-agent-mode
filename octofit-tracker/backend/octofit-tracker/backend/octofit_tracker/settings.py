6b0069bee2abc3e8c7ac8d99e8f54346d4c57722
anajuliakucharski <136736529+anajuliakucharski@users.noreply.github.com>
2025-04-27 21:01:37 +0000

    corrigindo erros

--- octofit-tracker/backend/octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit-tracker/backend/octofit_tracker/settings.py
@@ -74,8 +74,17 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "djongo",
+        "NAME": "octofit_db",
+        "ENFORCE_SCHEMA": False,
+        "CLIENT": {
+            "host": "localhost",
+            "port": 27017,
+            # "username": "<seu_usuario>",
+            # "password": "<sua_senha>",
+            # "authSource": "admin",
+            # "authMechanism": "SCRAM-SHA-1",
+        },
     }
 }
 
