bdc374fff321c1983d701519a238604723e25bae
anajuliakucharski <136736529+anajuliakucharski@users.noreply.github.com>
2025-04-27 23:46:02 +0000

    populate changes

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,32 +1,23 @@
-from django.core.management.base import BaseCommand # type: ignore
+from django.core.management.base import BaseCommand
 from octofit_tracker import test_data
-from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from pymongo import MongoClient
 
 class Command(BaseCommand):
-    help = 'Popula o banco de dados MongoDB com dados de teste.'
+    help = 'Popula o banco octofit_db com dados de teste.'
 
     def handle(self, *args, **options):
-        # Limpa as coleções antes de inserir
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
-
-        # Popula usuários
-        for user in test_data.users:
-            User.objects.create(**user)
-        # Popula times
-        for team in test_data.teams:
-            Team.objects.create(**team)
-        # Popula atividades
-        for activity in test_data.activities:
-            Activity.objects.create(**activity)
-        # Popula leaderboard
-        for entry in test_data.leaderboard:
-            Leaderboard.objects.create(**entry)
-        # Popula workouts
-        for workout in test_data.workouts:
-            Workout.objects.create(**workout)
-
-        self.stdout.write(self.style.SUCCESS('Banco de dados populado com sucesso!'))
+        client = MongoClient('localhost', 27017)
+        db = client['octofit_db']
+        # Limpa as coleções
+        db.users.delete_many({})
+        db.teams.delete_many({})
+        db.activity.delete_many({})
+        db.leaderboard.delete_many({})
+        db.workouts.delete_many({})
+        # Insere os dados de teste
+        db.users.insert_many(test_data.users)
+        db.teams.insert_many(test_data.teams)
+        db.activity.insert_many(test_data.activity)
+        db.leaderboard.insert_many(test_data.leaderboard)
+        db.workouts.insert_many(test_data.workouts)
+        self.stdout.write(self.style.SUCCESS('Banco octofit_db populado com dados de teste!'))
